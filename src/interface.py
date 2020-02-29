cf015ba5080a534ef2a60c814436147493c3f4e7
rossbroom99 <rossbroomfield@btinternet.com>
2020-02-29 18:54:47 +0000

    initial commit
    outline

---
+++ src/interface.py
@@ -0,0 +1,16 @@
+
+running = false
+
+def main():
+    
+    init()
+
+    while running == true:
+        #check web directory for alexa commands and website command
+        #read characteristics from nrf
+        #if something has changed write the change to file on web directory
+        #sleep for some time
+        
+def init():
+    #setup ble and connect to board
+    
